a37fa92af2349416b9c0b423b582592b581f5031
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2020-06-16 14:20:22 +0530

    Add files via upload

---
+++ Javascript/ATM.js
@@ -0,0 +1,51 @@
+/*
+There is enough money available on ATM in nominal value 10, 20, 50, 100, 200 and 500 dollars.
+
+You are given money in nominal value of n with 1<=n<=1500.
+
+Try to find minimal number of notes that must be used to repay in dollars, or output -1 if it is impossible
+
+
+*/
+
+function solve(n) {
+
+let count = 0;
+while(n>=500)
+{
+count++;
+n=n-500;
+}
+
+while(n>=200)
+{
+count++;
+n=n-200;
+}
+
+while(n>=100)
+{
+count++;
+n=n-100;
+}
+
+while(n>=50)
+{
+count++;
+n=n-50;
+}
+
+while(n>=20) 
+{
+count++;
+n=n-20;
+}
+  
+while(n>=10)
+{
+count++;
+n=n-10;
+}
+
+return n===0?count:-1;
+}
