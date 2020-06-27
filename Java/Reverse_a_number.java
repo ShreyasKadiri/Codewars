b7d7ea73645eb7a13b4e60508a527b2540a9b555
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2020-06-27 23:41:13 +0530

    Add files via upload

---
+++ Java/Reverse_a_number.java
@@ -0,0 +1,22 @@
+/*
+Given a number, write a function to output its reverse digits. (e.g. given 123 the answer is 321)
+
+Numbers should preserve their sign; i.e. a negative number should still be negative when reversed.
+
+Examples
+ 123 ->  321
+-456 -> -654
+1000 ->    1
+
+*/
+
+public class ReverseNumber {
+public static int reverse(int number) {
+int rev=0;
+while(number!=0){
+rev=rev*10+number%10;
+number=number/10;
+}
+return rev;
+}
+}
