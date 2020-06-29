f91622d40b483f042ea033bcaaad634862a1b402
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2020-06-29 12:57:09 +0530

    Add files via upload

---
+++ Javascript/Which_triangle_is_that.js
@@ -0,0 +1,23 @@
+/*
+Build a function that will take the length of each side of a triangle and return if it's either an Equilateral, an Isosceles, a Scalene 
+or an invalid triangle.
+
+It has to return a string with the type of triangle.
+For example:
+
+typeOfTriangle(2,2,1) --> "Isosceles"
+
+*/
+
+var typeOfTriangle = function (sideA, sideB, sideC) {
+if((sideA+sideB>sideC)&&(sideB+sideC>sideA)&&(sideC+sideA>sideB))
+{        
+if((sideA==sideB)&&(sideB==sideC))
+return "Equilateral";
+else if((sideA==sideB)||(sideB==sideC)||(sideA==sideC))
+return "Isosceles";
+else 
+return "Scalene";
+}
+return "Not a valid triangle";
+}
