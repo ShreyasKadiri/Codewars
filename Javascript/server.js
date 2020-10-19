7f884c4360ae05cc1551390cef2417c0b6837666
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2020-10-19 22:23:05 +0530

    Create server.js

---
+++ Javascript/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+const db = require('./config/keys.js').mongoURI;
+
+mongoose
+ .connect(db)
+ .then (() => console.log("MongoDB Connected"))
+ .catch(err => console.log(err));
+
+ const port = process.env.PORT || 5000;
+ app.listen(port, () => console.log(`Server listening on ${port}`));
